0c2a0c495c200568d2ff776122bfc11e2e8d6ae8
Данила Червонный <webmaster.pico@gmail.com>
2019-04-13 20:30:02 +0300

    Add project files.

---
+++ ClassifierAlgorithms.GUI/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿#region Usings
+
+using System;
+using System.ComponentModel;
+using DevExpress.Mvvm;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+#endregion
+
+namespace ClassifierAlgorithms.GUI.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase, IDataErrorInfo
+    {
+        private const string ScatterSeriesTag = "ScatterSeries";
+
+        public MainWindowViewModel()
+        {
+            PlotModel = InitializePlot(0, 1);
+        }
+
+        public PlotModel PlotModel { get; }
+
+        private PlotModel InitializePlot(int min, int max)
+        {
+            var plotModel = new PlotModel();
+            plotModel.Axes.Add(new LinearAxis {Position = AxisPosition.Left, Maximum = max, Minimum = min});
+            plotModel.Axes.Add(new LinearAxis {Position = AxisPosition.Bottom, Maximum = max, Minimum = min});
+
+            plotModel.Series.Add(new ScatterSeries
+                                 {
+                                     Tag = ScatterSeriesTag,
+                                     MarkerType = MarkerType.Cross,
+                                     MarkerSize = 2,
+                                     MarkerFill = OxyColors.Transparent,
+                                     MarkerStrokeThickness = 1,
+                                     MarkerStroke = OxyColors.DarkBlue
+                                 });
+            plotModel.Series.Add(new LineSeries
+                                 {
+                                     Tag = ScatterSeriesTag,
+                                     MarkerType = MarkerType.None
+                                 });
+
+            return plotModel;
+        }
+
+        #region IDataErrorInfo
+
+        public string this[string columnName]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Error { get; }
+
+        #endregion
+    }
+}
